2745e2408b0848cfef38402c2e962e9f0f9f50be
Aliya Safina <ktotamy@gmail.com>
2020-08-03 10:09:10 +0300

    content select

---
+++ src/components/base/content-select.js
@@ -0,0 +1,47 @@
+import React, {useEffect} from 'react';
+
+const ContentSelect = ({options, onChangeSelect, currentStatus}) => {
+    const handleSelectClick = function (event) {
+        const self = event.target;
+
+        if (!self.classList.contains('selected')) {
+            onChangeSelect(self.dataset.value);
+        }
+    };
+
+    const closeDropdown = (event) => {
+        const select = document.querySelector('.custom-select');
+        if (!select.contains(event.target)) {
+            select.classList.remove('open');
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('click', closeDropdown);
+        return () => window.removeEventListener('click', closeDropdown);
+    }, []);
+
+    return (
+        <div className="custom-select-wrapper"
+             onClick={() => document.querySelector('.custom-select').classList.toggle('open')}>
+            <div className="custom-select">
+                <div className="custom-select__trigger"><span>{options.find(option => option.value === currentStatus).name}</span>
+                    <div className="arrow"/>
+                </div>
+                <div className="custom-options" onClick={handleSelectClick}>
+                    {
+                        options.map(option => (
+                            <span
+                                key={option.key}
+                                className={option.value === currentStatus ? "custom-option selected" : "custom-option"}
+                                data-value={option.value}>
+                                {option.name}
+                            </span>))
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContentSelect;
