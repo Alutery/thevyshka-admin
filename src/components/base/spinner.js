d4bbf50fcb9626f792dc9bcf4233426b30702f5b
Aliya Safina <ktotamy@gmail.com>
2020-08-03 10:08:23 +0300

    spinner component

---
+++ src/components/base/spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div className="spinner-wrapper">
+            <span className="spinner"/>
+        </div>
+    );
+};
+
+export default Spinner;
